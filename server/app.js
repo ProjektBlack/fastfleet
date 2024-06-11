46107be018e397bced5577babba3570c83c21e1a
ProjektBlack <jaden.sandiego@gmail.com>
2024-06-11 22:25:05 +0800

    jsd/backend/core/Added environment variables

--- server/app.js
+++ server/app.js
@@ -1,7 +1,15 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+require('dotenv').config({ path: `./environment/.env.${process.env.NODE_ENV}` });
 
+const express = require('express');
+// const cors = require('cors');
+
+const app = express();
+
+const port = process.env.PORT;
+
+// app.use(cors());
+
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
